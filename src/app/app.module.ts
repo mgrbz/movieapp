4daa522b9f177ea837fe24b078eb3a491958b631
grbz <mgurbuzy@gmail.com>
2022-05-10 00:34:46 +0300

    project components generated
    generate command: ng g c 'component-name' it is created to into app folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CategoryComponent } from './components/category/category.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { MovieComponent } from './components/movies/movie/movie.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [  //components
+    AppComponent,
+    NavbarComponent,
+    CategoryComponent,
+    MoviesComponent,
+    MovieComponent,
+    MovieDetailsComponent,
+    FooterComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [    // Module
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],  //services
+  bootstrap: [AppComponent]   //starter components
 })
 export class AppModule { }
