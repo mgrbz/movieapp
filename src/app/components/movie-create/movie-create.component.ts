28b6660a2259a3f28fe6021a37d090671e5c6631
grbz <muhammet.gurbuz@ibtech.com.tr>
2022-05-15 16:37:42 +0300

    create movie step 3 manuel validations

--- src/app/components/movie-create/movie-create.component.ts
+++ src/app/components/movie-create/movie-create.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { Router } from "@angular/router";
+import { AlertifyService } from "src/services/alertify.service";
 import { CategoryService } from "src/services/category.service";
 import { MovieCreateService } from "src/services/movie-create.service";
 import { ICategory } from "src/types/category";
@@ -18,7 +19,12 @@ export class MovieCreateComponent{
   error: string= ''
   title: string = 'MovieC Create';
   categories: ICategory[] = [];
-  constructor(private movieCreateService: MovieCreateService, private categoryService: CategoryService, private router: Router){}
+  constructor(
+    private movieCreateService: MovieCreateService,
+    private categoryService: CategoryService,
+    private router: Router,
+    private alertify: AlertifyService
+    ){}
 
   ngOnInit() {
     this.categoryService.getCategories().subscribe(data => {
@@ -27,6 +33,32 @@ export class MovieCreateComponent{
   }
 
   createMovie(title: any, description: any, imageUrl: any, categoryId: any ) {
+    const imageExtention = ["jpeg","jpg","png"];
+
+    if(title.value === '' || description.value === '' || imageUrl.value === '' || categoryId.value === '-1' ){
+      this.alertify.error('Tüm Alanları Doldurmanız gereklemtedir!');
+      return;
+    }
+
+    if(title.value.length < 5) {
+      this.alertify.error('Title çin min 5 karakter girmelisiniz!');
+      return;
+    }
+
+    if(description.value.length < 10 || description.value.length > 50){
+      this.alertify.error('Description alanı min 10 max 50 karakter olmalıdır');
+      return;
+    }
+    
+    const imageType = imageUrl.value.split('.').pop();
+    if(imageExtention.indexOf(imageType) === -1){
+      this.alertify.error('resim uzantınız doğru olmalıdır');
+      return;
+    }
+
+
+
+
 
     const movie = {
       id: 0,
