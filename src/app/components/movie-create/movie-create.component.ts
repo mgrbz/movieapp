23a6234e084d742191c2ee7e9557be87d914a0b9
grbz <muhammet.gurbuz@ibtech.com.tr>
2022-05-15 20:04:39 +0300

    create movie step 4

--- src/app/components/movie-create/movie-create.component.ts
+++ src/app/components/movie-create/movie-create.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
 import { Router } from "@angular/router";
 import { AlertifyService } from "src/services/alertify.service";
 import { CategoryService } from "src/services/category.service";
@@ -19,6 +20,16 @@ export class MovieCreateComponent{
   error: string= ''
   title: string = 'MovieC Create';
   categories: ICategory[] = [];
+  formModel: IMovie = {
+    id: 0,
+    title: "",
+    description: "",
+    imageUrl: "",
+    isPopular: false,
+    categoryId: 0,
+    datePublished: 0
+  };
+
   constructor(
     private movieCreateService: MovieCreateService,
     private categoryService: CategoryService,
@@ -32,52 +43,32 @@ export class MovieCreateComponent{
     })
   }
 
-  createMovie(title: any, description: any, imageUrl: any, categoryId: any ) {
-    const imageExtention = ["jpeg","jpg","png"];
+  createMovie(form: NgForm) {
 
-    if(title.value === '' || description.value === '' || imageUrl.value === '' || categoryId.value === '-1' ){
-      this.alertify.error('Tüm Alanları Doldurmanız gereklemtedir!');
-      return;
-    }
-
-    if(title.value.length < 5) {
-      this.alertify.error('Title çin min 5 karakter girmelisiniz!');
-      return;
-    }
-
-    if(description.value.length < 10 || description.value.length > 50){
-      this.alertify.error('Description alanı min 10 max 50 karakter olmalıdır');
-      return;
-    }
-    
-    const imageType = imageUrl.value.split('.').pop();
-    if(imageExtention.indexOf(imageType) === -1){
-      this.alertify.error('resim uzantınız doğru olmalıdır');
-      return;
-    }
+    console.log('fromModel : ', this.formModel)
+    console.log('ngForm : ', form.value)
 
 
 
 
+  //   const movie = {
+  //     id: 0,
+  //     title: title.value,
+  //     description: description.value,
+  //     imageUrl: imageUrl.value,
+  //     categoryId: categoryId.value,
+  //     isPopular: false,
+  //     datePublished: new Date().getTime()
+  //   };
 
-    const movie = {
-      id: 0,
-      title: title.value,
-      description: description.value,
-      imageUrl: imageUrl.value,
-      categoryId: categoryId.value,
-      isPopular: false,
-      datePublished: new Date().getTime()
-    };
-
-    this.movieCreateService.createMovie(movie).subscribe(data => {
-      // this.router.navigate(['/movies']);
-      this.router.navigate([`/movies/${data.id}`]);
-    },
-    error => {
-      this.error = error
-    }
-    )
+  //   this.movieCreateService.createMovie(movie).subscribe(data => {
+  //       // this.router.navigate(['/movies']);
+  //       this.router.navigate([`/movies/${data.id}`]);
+  //     },
+  //     error => {
+  //       this.error = error
+  //     }
+  //   )
 
 
   }
@@ -85,4 +76,30 @@ export class MovieCreateComponent{
 
 }
 
+
+
+
+
+  //   const imageExtention = ["jpeg","jpg","png"];
+
+  //   if(title.value === '' || description.value === '' || imageUrl.value === '' || categoryId.value === '-1' ){
+  //     this.alertify.error('Tüm Alanları Doldurmanız gereklemtedir!');
+  //     return;
+  //   }
+
+  //   if(title.value.length < 5) {
+  //     this.alertify.error('Title çin min 5 karakter girmelisiniz!');
+  //     return;
+  //   }
+
+  //   if(description.value.length < 10 || description.value.length > 50){
+  //     this.alertify.error('Description alanı min 10 max 50 karakter olmalıdır');
+  //     return;
+  //   }
+
+  //   const imageType = imageUrl.value.split('.').pop();
+  //   if(imageExtention.indexOf(imageType) === -1){
+  //     this.alertify.error('resim uzantınız doğru olmalıdır');
+  //     return;
+  //   }
 
